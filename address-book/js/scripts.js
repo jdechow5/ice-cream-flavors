3fb82da7739e72e9a068d723344d295f40aa6f84
Justin Dechow <justin.dechow@gmail.com>
2022-01-24 08:57:27 -0800

    WIP: Building out address-book functionality

---
+++ address-book/js/scripts.js
@@ -0,0 +1,20 @@
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function (contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
+};
